9003c652fac33bbe1ba4c9a40b4b468969ec4b14
VaibhavD13 <115723003+VaibhavD13@users.noreply.github.com>
2024-07-06 18:02:27 +0000

    learned variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 23425
+let accEmail="vaibhav@gmail.com"
+// var accPass="112211"   not using due to scope issue
+accountCity ="wardha"
+
+// accountId=23 // not allowed
+accEmail="vaibhav@d.com"
+accPass="223322"
+accountCity ="Nagpur"
+
+// console.log(accountId) /*normal way one by one*/
+console.table([accEmail,accPass,accountCity]) /*grouping way*/
