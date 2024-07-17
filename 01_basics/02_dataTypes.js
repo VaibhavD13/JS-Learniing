0851bf56234a1ccfdb8e8510b9b9ddc912e9a79f
VaibhavD13 <115723003+VaibhavD13@users.noreply.github.com>
2024-07-17 18:38:16 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat js and newer version
+
+// alert(3+3) we are using nodejs not browser so will get error by using this
+
+let name="Hitesh"
+let age=18
+
+console.log(typeof name)
+console.log(typeof age)
