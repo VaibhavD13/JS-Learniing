47bb8e30fa747b3a9baa2e348b301d6e0ab0ab83
VaibhavD13 <115723003+VaibhavD13@users.noreply.github.com>
2024-07-18 16:54:32 +0000

    learned conversions and operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,14 +1,39 @@
-let score= "11abcd"
+// let score= "11abcd"
 
-console.log(typeof score)
+// // console.log(typeof score)
 
-console.log(typeof (score))
+// // console.log(typeof (score))
 
-let valueInNumber= Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber= Number(score)
+// // console.log(typeof valueInNumber)
+// // console.log(valueInNumber)
 
-let someNumber=33
-let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33
+// let stringNumber= String(someNumber)
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
+
+// // *********************Operations**********************************
+
+// let value=3
+// let negValue=- value
+// console.log(negValue);
+
+
+// let str1="hello "
+// let str2="vaibhav"
+// let str3= str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2)
+// console.log(1 + 2 + "2")
+// console.log((1 + 2) * 2)  //use parenthesis for betteer code writing an dont be dependent on static thins
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
